be5eecb19c496d5a0727f81489ac523ec1bf4fbf
Raden-nvii <navinugraha15@gmail.com>
2024-07-29 15:15:50 +0700

    feat: add list task on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const listTask = [
+    { id: 1, text: "Go to TETO" },
+    { id: 2, text: "Go home" },
+    { id: 3, text: "Bootcamp task" },
+  ];
+
   return (
     <div>
-      <h1>Proker: Productivity Tracker</h1>
+      <h1>Todo List</h1>
+
+      <ul>
+        {listTask.map((task) => (
+          <li key={task.id}>
+            <span>{task.text}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
